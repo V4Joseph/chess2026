3c74812159d8319dc513298cf51669da1d32d9a6
Austin <austinjolong@gmail.com>
2026-02-28 23:57:38 -0700

    working more on register endpoint

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,24 @@
 package server;
 
+import com.google.gson.Gson;
+import dataaccess.*;
 import io.javalin.*;
 import io.javalin.http.Context;
+import service.RR_Classes.RegisterResult;
+import service.UserService;
+import service.RR_Classes.RegisterRequest;
 
 
 public class Server {
 
     private final Javalin javalin;
+    UserDataAccess userDataAccess = new UserDataMem();
+    AuthDataAccess authDataAccess = new AuthDataMem();
+    UserService userService = new UserService(userDataAccess, authDataAccess);
 
     public Server() {
         javalin = Javalin.create(config -> config.staticFiles.add("web"))
-//                .post("/user",this::register)
+                .post("/user",this::register)
 
 
 
@@ -29,7 +37,19 @@ public class Server {
         javalin.stop();
     }
 
-    private void register(Context context) {
+    private void register(Context context) throws DataAccessException {
+        RegisterRequest registerRequest = getBodyObject(context,RegisterRequest.class);
+        RegisterResult registerResult = userService.register(registerRequest);
+        String json = new Gson().toJson(registerResult);
+        context.json(json);
 
+    }
+
+    private static <T> T getBodyObject(Context context, Class<T> givenClass) {
+        var bodyObject = new Gson().fromJson(context.body(),givenClass);
+        if (bodyObject == null) {
+            throw new RuntimeException("Empty body");
+        }
+        return bodyObject;
     }
 }
