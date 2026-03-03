3fc969ba1bf7d626285b10a93d30f58f6df631a5
Austin <austinjolong@gmail.com>
2026-03-02 17:07:07 -0700

    fixed nesting

--- shared/src/main/java/chess/PawnMovesCalculator.java
+++ shared/src/main/java/chess/PawnMovesCalculator.java
@@ -22,37 +22,52 @@ public class PawnMovesCalculator implements PieceMovesCalculator{
             int newRow = row + step;
             int newCol = col + mov;
             if (newCol >=1 && newCol < 9) {
-                ChessPosition newPosition = new ChessPosition(newRow, newCol);
-                ChessPiece targetPiece = board.getPiece(newPosition);
-                if (mov == 0 && targetPiece == null) {
-                        if (newRow == 8 || newRow == 1) {
-                            for (ChessPiece.PieceType p : promotionList) {
-                                possibleMoves.add(new ChessMove(myPosition,newPosition,p));
-                            }
-                        }
-                        else {
-                            possibleMoves.add(new ChessMove(myPosition, newPosition, null));
-                        }
-                        if (myPiece.getTeamColor() == ChessGame.TeamColor.WHITE &&
-                            row == 2 ||
-                            myPiece.getTeamColor() == ChessGame.TeamColor.BLACK &&
-                            row == 7) {
-                            newRow = row + (step*2);
-                            newPosition = new ChessPosition(newRow,col);
-                            targetPiece = board.getPiece(newPosition);
-                            if (targetPiece == null) {
-                                possibleMoves.add(new ChessMove(myPosition,newPosition,null));
-                            }
-                        }
-                } else if (mov != 0){
-                    diagonalMoves(myPosition, targetPiece, myPiece, newRow, promotionList, possibleMoves, newPosition);
-                }
+                movePawn(board, myPosition, mov, newRow, newCol, promotionList, possibleMoves, myPiece, row, step, col);
             }
         }
         return possibleMoves;
     }
 
-    private static void diagonalMoves(ChessPosition myPosition, ChessPiece targetPiece, ChessPiece myPiece, int newRow, ChessPiece.PieceType[] promotionList, List<ChessMove> possibleMoves, ChessPosition newPosition) {
+    private static void movePawn(ChessBoard board, ChessPosition myPosition, int mov, int newRow, int newCol, ChessPiece.PieceType[] promotionList, List<ChessMove> possibleMoves, ChessPiece myPiece, int row, int step, int col) {
+        ChessPosition newPosition = new ChessPosition(newRow, newCol);
+        ChessPiece targetPiece = board.getPiece(newPosition);
+        if (mov == 0 && targetPiece == null) {
+            forwardMove(board, myPosition, newRow, promotionList, possibleMoves, newPosition, myPiece, row, step, col);
+        } else if (mov != 0){
+            diagonalMoves(myPosition, targetPiece, myPiece, newRow, promotionList, possibleMoves, newPosition);
+        }
+    }
+
+    private static void forwardMove(ChessBoard board, ChessPosition myPosition, int newRow, ChessPiece.PieceType[] promotionList, List<ChessMove> possibleMoves, ChessPosition newPosition, ChessPiece myPiece, int row, int step, int col) {
+        ChessPiece targetPiece;
+        if (newRow == 8 || newRow == 1) {
+            for (ChessPiece.PieceType p : promotionList) {
+                possibleMoves.add(new ChessMove(myPosition, newPosition,p));
+            }
+        }
+        else {
+            possibleMoves.add(new ChessMove(myPosition, newPosition, null));
+        }
+        if (myPiece.getTeamColor() == ChessGame.TeamColor.WHITE &&
+            row == 2 ||
+            myPiece.getTeamColor() == ChessGame.TeamColor.BLACK &&
+            row == 7) {
+            newRow = row + (step *2);
+            newPosition = new ChessPosition(newRow, col);
+            targetPiece = board.getPiece(newPosition);
+            if (targetPiece == null) {
+                possibleMoves.add(new ChessMove(myPosition, newPosition,null));
+            }
+        }
+    }
+
+    private static void diagonalMoves(ChessPosition myPosition,
+                                      ChessPiece targetPiece,
+                                      ChessPiece myPiece,
+                                      int newRow,
+                                      ChessPiece.PieceType[] promotionList,
+                                      List<ChessMove> possibleMoves,
+                                      ChessPosition newPosition) {
         if (targetPiece != null && targetPiece.getTeamColor() != myPiece.getTeamColor()) {
             if (newRow == 8 || newRow == 1) {
                 for (ChessPiece.PieceType p : promotionList) {
