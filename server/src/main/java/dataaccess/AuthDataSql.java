797b0899584a44269b1ec4b721a68a8557edd680
Austin <austinjolong@gmail.com>
2026-03-11 11:47:32 -0600

    Created Sql Auth DAO

---
+++ server/src/main/java/dataaccess/AuthDataSql.java
@@ -0,0 +1,24 @@
+package dataaccess;
+
+import com.google.gson.Gson;
+import model.AuthData;
+
+import java.sql.*;
+
+public class AuthDataSql implements AuthDataAccess{
+    public AuthDataSql() throws DataAccessException {
+
+    }
+    public AuthData createAuth(AuthData authData){
+
+    }
+    public AuthData getAuth(String authToken) throws DataAccessException {
+
+    }
+    public void deleteAuth(String authToken) {
+
+    }
+    public void clearAuths(){
+
+    }
+}
