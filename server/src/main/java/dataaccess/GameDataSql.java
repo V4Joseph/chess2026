844b08265335d96985305576abbabb971c682420
Austin <ajl79@byu.edu>
2026-03-11 21:25:27 -0600

    finished writing game data sql

--- server/src/main/java/dataaccess/GameDataSql.java
+++ server/src/main/java/dataaccess/GameDataSql.java
@@ -6,6 +6,7 @@ import model.AuthData;
 import model.GameData;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import static java.sql.Types.NULL;
@@ -20,16 +21,50 @@ public class GameDataSql implements GameDataAccess{
     }
 
     public GameData createGame(String gameName) throws DataAccessException {
-
+        var statement = "INSERT INTO gameData (whiteUsername, blackUsername, gameName, json) VALUES (?,?,?,?)";
+        ChessGame game = new ChessGame();
+        String json = new Gson().toJson(game);
+        int gameID = executeUpdate(statement,null,null,gameName,json);
+        return new GameData(gameID,null,null,gameName,game);
     }
     public GameData getGame(int gameID) throws DataAccessException {
-
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT gameID, whiteUsername, blackUsername, gameName FROM gameData WHERE gameID=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setInt(1,gameID);
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        return readGame(rs);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+        return null;
     }
     public Collection<GameData> listGames() throws DataAccessException {
-
+        Collection<GameData> gameList = new ArrayList<>();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT gameID, whiteUsername, blackUsername, gameName FROM gameData";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                try (ResultSet rs = ps.executeQuery()) {
+                    if (rs.next()) {
+                        gameList.add(readGame(rs));
+                    }
+                }
+            }
+        } catch (Exception e) {
+            throw new DataAccessException(e.getMessage());
+        }
+        return gameList;
     }
     public void updateGame(int gameID, GameData gameData) throws DataAccessException {
-
+        var statement = "DELETE FROM gameData WHERE gameID=?";
+        executeUpdate(statement,gameID);
+        statement = "INSERT INTO gameData (whiteUsername, blackUsername, gameName, json) VALUES (?,?,?,?)";
+        String json = new Gson().toJson(gameData.game());
+        executeUpdate(statement,gameData.whiteUsername(),gameData.blackUsername(),gameData.gameName(),json);
     }
     public void clearGames() throws DataAccessException {
         var statement = "TRUNCATE gameData";
@@ -46,7 +81,7 @@ public class GameDataSql implements GameDataAccess{
         return gameData;
     }
 
-    private void executeUpdate(String statement, Object... params) throws DataAccessException{
+    private int executeUpdate(String statement, Object... params) throws DataAccessException{
         try (Connection conn = DatabaseManager.getConnection()) {
             try (PreparedStatement ps = conn.prepareStatement(statement, Statement.RETURN_GENERATED_KEYS)) {
                 for (int i = 0; i < params.length; i++) {
@@ -55,6 +90,12 @@ public class GameDataSql implements GameDataAccess{
                     else if (param == null) ps.setNull(i + 1, NULL);
                 }
                 ps.executeUpdate();
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+
+                return 0;
             }
         } catch (SQLException e) {
             throw new DataAccessException(e.getMessage());
@@ -65,8 +106,8 @@ public class GameDataSql implements GameDataAccess{
             """
         CREATE TABLE IF NOT EXISTS gameData (
         'gameID' int NOT NULL AUTO_INCREMENT,
-        'whiteUsername' varchar(256) NOT NULL,
-        'blackUsername' varchar(256) NOT NULL,
+        'whiteUsername' varchar(256),
+        'blackUsername' varchar(256),
         'gameName' varchar(256) NOT NULL,
         'json' TEXT DEFAULT NULL,
         PRIMARY KEY ('gameID'),
