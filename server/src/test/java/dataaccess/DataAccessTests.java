dd17dbd5beaf443693f4ced36afec85970fcce4b
Austin <ajl79@byu.edu>
2026-03-12 18:26:37 -0600

    working on Data Tests

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,5 +1,29 @@
 package dataaccess;
 
-public class DataAccessTests {
+import model.AuthData;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
+public class DataAccessTests {
+    AuthDataAccess authDataAccess = new AuthDataSql();
+
+    // Auth Tests
+    @Test
+    @DisplayName("Create Auth S")
+    public void createAuthS() {
+        AuthData authData = new AuthData("sigma","boi");
+        try {
+            authDataAccess.createAuth(authData);
+        } catch (Exception e) {
+            throw new RuntimeException("Failure to create auth");
+        }
+        var statement = "SELECT authToken, username FROM authdata WHERE authToken=?";
+
+    }
+
+    @Test
+    @DisplayName("Create Auth F")
+    public void createAuthF() {
+
+    }
 }
