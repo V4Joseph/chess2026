95dbe1b276c3318c725ffae64d3af5cd05617750
Austin <austinjolong@gmail.com>
2026-03-15 18:33:38 -0600

    Phase 5 Getting Started

--- server/src/test/java/dataaccess/DataAccessTests.java
+++ server/src/test/java/dataaccess/DataAccessTests.java
@@ -1,11 +1,23 @@
 package dataaccess;
 
 import model.AuthData;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class DataAccessTests {
     AuthDataAccess authDataAccess = new AuthDataSql();
+
+
+    @BeforeAll
+    public static void setUp() {
+
+    }
 
     // Auth Tests
     @Test
@@ -17,7 +29,20 @@ public class DataAccessTests {
         } catch (Exception e) {
             throw new RuntimeException("Failure to create auth");
         }
-        var statement = "SELECT authToken, username FROM authdata WHERE authToken=?";
+        try (Connection conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT authToken, username FROM authdata WHERE authToken=?";
+            try (PreparedStatement ps = conn.prepareStatement(statement)) {
+                ps.setString(1,authData.authToken());
+                try (ResultSet rs = ps.executeQuery()) {
+                    Assertions.assertTrue(rs.next(),"Not Found");
+                    AuthData authData2 = new AuthData(rs.getString("authToken"),rs.getString("username"));
+                    Assertions.assertEquals(authData.authToken(),authData2.authToken());
+                    Assertions.assertEquals(authData.username(),authData2.username());
+                }
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
 
     }
 
