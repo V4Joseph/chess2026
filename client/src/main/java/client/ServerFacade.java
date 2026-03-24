cfc087633e8f193f83998303a2ce2467b7d2eec6
Austin <austinjolong@gmail.com>
2026-03-24 11:47:37 -0600

    Starting Server Facade and Client work'

---
+++ client/src/main/java/client/ServerFacade.java
@@ -0,0 +1,7 @@
+package client;
+import server.src.main.java.service.requestsandresults.*;
+
+public class ServerFacade {
+    public RegisterResult register(RegisterRequest registerRequest) {
+    }
+}
---
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -0,0 +1,44 @@
+package client;
+
+import org.junit.jupiter.api.*;
+import server.Server;
+import model.*;
+import service.requestsandresults.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ServerFacadeTests {
+
+    private static Server server;
+    static ServerFacade facade;
+
+    @BeforeAll
+    public static void init() {
+        server = new Server();
+        var port = server.run(0);
+        System.out.println("Started test HTTP server on " + port);
+        facade = new ServerFacade(port);
+    }
+
+    @AfterAll
+    static void stopServer() {
+        server.stop();
+    }
+
+    @BeforeEach
+    public void() {
+
+    }
+
+
+    @Test
+    public void sampleTest() {
+        assertTrue(true);
+    }
+
+    @Test
+    void register() throws Exception {
+        var authData = facade.register("player1", "password", "p1@email.com");
+        assertTrue(authData.authToken().length() > 10);
+    }
+}
